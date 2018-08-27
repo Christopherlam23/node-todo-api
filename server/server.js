a064e10c07bc5c1f4a6fcd5d517de2d6cd4257de
Christopher Lam <christopherlam@live.com>
2018-08-27 08:40:54 -0700

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,14 @@ app.post('/todos', (req, res) => {
 	});
 });
 
+app.get('/todos', (req, res) => {
+	Todo.find().then((allTodos) => {
+		res.send({allTodos});
+	}, (err) => {
+		res.status(400).send(err);
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}`);
 });
