ffd5100cacd448b3ca480857d00767270d93ba64
Christopher Lam <christopherlam@live.com>
2018-08-30 11:46:08 -0700

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -57,12 +57,12 @@ app.delete('/todos/:id', (req, res) => {
 		return res.status(404).send();
 	}
 
-	Todo.findByIdAndRemove(id).then((todoDoc) => {
-		if(!todoDoc){
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if(!todo){
 			return res.status(404).send();
 		}
 
-		res.send(todoDoc);
+		res.send({todo});
 	}).catch((err) => {
 		res.status(400).send();
 	});
