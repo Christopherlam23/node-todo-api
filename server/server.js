ccdc4b6127aeee78f984fd2e9ee4bc421258827f
Christopher Lam <christopherlam@live.com>
2018-08-30 11:13:44 -0700

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -50,6 +50,24 @@ app.get('/todos/:id', (req, res) => {
 	
 });
 
+app.delete('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	Todo.findByIdAndRemove(id).then((todoDoc) => {
+		if(!todoDoc){
+			return res.status(404).send();
+		}
+
+		res.send(todoDoc);
+	}).catch((err) => {
+		res.status(400).send();
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}`);
 });
